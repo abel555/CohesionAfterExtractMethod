38a931a6d1f2bbd0e68544d55f8646524e53745d
unknown <taqui555@gmail.com>
2020-04-17 12:15:50 -0400

    asd

--- src/org/method/ExtractMethodMiner.java
+++ src/org/method/ExtractMethodMiner.java
@@ -10,11 +10,11 @@ public class ExtractMethodMiner {
     public static void main(String[] args) throws Exception {
 
 
-        ExtractMethodProcessor extractMethodProcessor = new ExtractMethodProcessor("E:\\projectsinfo25.txt"); //args[1]
+        ExtractMethodProcessor extractMethodProcessor = new ExtractMethodProcessor("E:\\projectsinfoHolo.txt"); //args[1]
         List<String> repos = extractMethodProcessor.readUrlRepos("E:\\projects-urls.txt");
-        for (String rep:repos){
-        extractMethodProcessor.analizeProjects(rep); //argo[0]
-        }
+        /*for (String rep:repos){
+        }*/
+        extractMethodProcessor.analizeProjects("https://github.com/Prototik/HoloEverywhere.git"); //argo[0]
 
 
     }
