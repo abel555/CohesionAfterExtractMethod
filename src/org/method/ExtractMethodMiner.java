6bb94457e8cd502a87e978671e63a78289b556bb
abel555 <taqui555@gmail.com>
2020-07-29 13:06:44 -0400

    fa-in done

--- src/org/method/ExtractMethodMiner.java
+++ src/org/method/ExtractMethodMiner.java
@@ -7,11 +7,11 @@ public class ExtractMethodMiner {
     public static void main(String[] args) throws Exception {
         ExtractMethodProcessor extractMethodProcessor = new ExtractMethodProcessor("/Users/abel/Desktop/faninMuestra2.txt");
         List<String> repos = extractMethodProcessor.readUrlRepos("/Users/abel/Downloads/projects2.txt");
-
+/*
         for(String rep: repos){
             extractMethodProcessor.analizeProjects(rep);
-        }
-        //extractMethodProcessor.analizeProjects("https://github.com/h2oai/h2o-2.git");
+        }*/
+        extractMethodProcessor.analizeProjects("https://github.com/prontera/spring-cloud-rest-tcc.git");
         //extractMethodProcessor.analizeProjects("https://github.com/tyzlmjj/PagerBottomTabStrip.git");
         //extractMethodProcessor.analizeProjects("https://github.com/intuit/karate.git");
 
